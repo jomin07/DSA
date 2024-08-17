3334daaa26f1d684c60bc7eff62e13e5d030002c
jomin07 <107167492+jomin07@users.noreply.github.com>
2024-08-17 20:06:27 +0530

    minor recursive binary search updates

--- Recursion/recursiveBinarySearch.js
+++ Recursion/recursiveBinarySearch.js
@@ -1,23 +1,23 @@
-function recursiveBinarySearch(arr, target){
-    return search(arr, target, 0, arr.length - 1);
+function recursiveBinarySearch(arr, target) {
+  return search(arr, target, 0, arr.length - 1);
 }
 
-function search(arr, target, left, right){
-    if (left > right) {
-        return - 1;
-    }
-    let mid = Math.floor((left + right) / 2);
+function search(arr, target, left, right) {
+  if (left > right) {
+    return "Not Found";
+  }
+  let mid = Math.floor((left + right) / 2);
 
-    if (arr[mid] === target) {
-        return mid;
-    }
+  if (arr[mid] === target) {
+    return mid;
+  }
 
-    if (arr[mid] < target) {
-        return search(arr, target, mid + 1, right)
-    } else if(arr[mid] > target) {
-        return search(arr, target, left, mid - 1);
-    }
+  if (target < mid) {
+    return search(arr, target, left, mid - 1);
+  } else {
+    return search(arr, target, mid + 1, right);
+  }
 }
 
 //Array must be sorted
-console.log(recursiveBinarySearch([2,3,5,6],5));
+console.log(recursiveBinarySearch([2, 3, 5, 6], 53));
--- Recursion/sample.js
+++ Recursion/sample.js
@@ -1,8 +1,21 @@
-function palindrome(str) {
-  if (str.length < 2) return true;
-  if (str[0] !== str[str.length - 1]) return false;
-
-  return palindrome(str.slice(1, -1));
+function binarySearch(arr, target) {
+  return search(arr, target, 0, arr.length - 1);
 }
 
-console.log(palindrome("mam"));
+function search(arr, target, left, right) {
+  if (left > right) return "Not Found";
+
+  let mid = Math.floor((left + right) / 2);
+
+  if (target === arr[mid]) {
+    return mid;
+  }
+
+  if (target < mid) {
+    return search(arr, target, left, mid - 1);
+  } else {
+    return search(arr, target, mid + 1, right);
+  }
+}
+
+console.log(binarySearch([1, 2, 3, 4, 5], 55));
