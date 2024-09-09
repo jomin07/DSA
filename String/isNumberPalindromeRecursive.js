9211a36174a2064d6050ec04107a811c16b4dcf6
jomin07 <107167492+jomin07@users.noreply.github.com>
2024-09-09 20:14:38 +0530

    recursive palindrome minor updates

--- String/isNumberPalindromeRecursive.js
+++ String/isNumberPalindromeRecursive.js
@@ -1,12 +1,12 @@
 function isNumberPalindrome(n) {
-  function helper(n, rev = 0) {
-    if (n === 0) return rev;
-
-    let rem = n % 10;
-
-    return helper(Math.floor(n / 10), rev * 10 + rem);
-  }
   return helper(n) === n;
 }
 
+function helper(n, rev = 0) {
+  if (n === 0) return rev;
+
+  let rem = n % 10;
+
+  return helper(Math.floor(n / 10), rev * 10 + rem);
+}
 console.log(isNumberPalindrome(121));
