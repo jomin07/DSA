793af08807024393be5ae338912e6a4465b9882c
jomin07 <107167492+jomin07@users.noreply.github.com>
2024-10-31 22:51:43 +0530

    minor updates on maxOccurence hashTable problem

--- Hash Table/maxOccurence.js
+++ Hash Table/maxOccurence.js
@@ -14,6 +14,7 @@ function maxOccurrence(arr) {
     if (freq[key] > max1) {
       max2 = max1;
       max1 = freq[key];
+      max2Key = max1Key;
       max1Key = key;
     } else if (freq[key] > max2) {
       max2 = freq[key];
